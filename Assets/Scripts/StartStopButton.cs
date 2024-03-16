632af22999f3f4c0463b98a7d5dc818398e60c13
Chris_VR <cd.hardwick@gmail.com>
2024-03-16 22:11:59 +0000

    Rename StartButton Script to StartStopButton

--- Assets/Scripts/StartStopButton.cs
+++ Assets/Scripts/StartStopButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using realvirtual;
 
-public class StartButton : MonoBehaviour
+public class StartStopButton : MonoBehaviour
 {
 
  [Header("Factory Machine")]
@@ -12,12 +12,11 @@ public class StartButton : MonoBehaviour
     public string nodeID1; // stop
     public string nodeID0; // start
 
-    public void pushStartButton ()
+    public void pushStartStopButton ()
     {
         Debug.Log("buttonON");
         oPCUAinterface.WriteNodeValue(nodeID0, false);
         oPCUAinterface.WriteNodeValue(nodeID1, true);
-
 
     }
 }
