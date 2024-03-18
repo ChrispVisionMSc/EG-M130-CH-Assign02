f62523ed191bf2ea0a2ef47c043e9f3f962a11db
Chris_VR <cd.hardwick@gmail.com>
2024-03-18 12:32:55 +0000

    Add Virtual Twin Test

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVVteUtCSC8vLy8vQUFBQm1jaVptaWNqdEViMW1xbVR3K00xQldVOEgyVExYT1JsVThQaVh2Yks2elZ6NnczaXA4elVzNU00K1ZJMThhbVNFYTlsVU1MbzhndG1lTjNNV0RscTFVOWl6cDd6L3JmK1hKOWlsb1RTWklWSkNuUTZuRjNIVnZjbXRFeFhEQXFqRk10ZnZ3WDlieDF1R05zRVcrOTJCbStxQTBTdVp1Q3A0Z2ppbGJ0TWsyb1B0NUFpSnBGQXVpRDhPcW4zdk5pZk1yNmFsclBGZUx1WThKZ1QwNmVzVlc1c0ozV3crWThub3ZoSFNpVXphUDh2MUlpc3BWNHlIQ0N4dmtqSTFoSVowT2poRFh5YU1RQTQzcjVTdWV2blRmQkZnUE9vTyswc3BLZC9ZdlFLZ0U2dS80a3paS0YrR3ZBaDVaWmZxakVrcGFzOTJZN2tYTE9DTEFpN3drUTBPZCsyOC9yMUs1QlVuL2N2T2p2YzYvSlI=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
